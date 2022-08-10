d9e1996eef18bce70202d2f7788905f1e197d870
kostolaza <105263190+kostolaza@users.noreply.github.com>
2022-08-09 20:59:26 -0400

    added temp relevant icon

--- src/app.js
+++ src/app.js
@@ -35,16 +35,28 @@ function displayTemperature(response) {
     let humidityElement=document.querySelector("#humidity");
     let windElement=document.querySelector("#wind");
     let dateElement=document.querySelector("#date");
+    let iconElement=document.querySelector("#icon");
+    
+    celsiusTemperature = response.data.main.temp;
+    
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML=response.data.name;
     descriptionElement.innerHTML=response.data.weather[0].description;
     humidityElement.innerHTML=response.data.main.humidity;
     windElement.innerHTML=Math.round(response.data.wind.speed);
     dateElement.innerHTML=formatDate(response.data.dt * 1000);
+    iconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      );
+      iconElement.setAttribute("alt", response.data.weather[0].description);
+    
+      getForecast(response.data.coord);
 }
 
 let apiKey = "8eaa10e0ab5463f9893094121b6b5dc5";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=8eaa10e0ab5463f9893094121b6b5dc5&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
 
