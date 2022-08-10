91f3e31fd6217e897a73017da95b5c589d86491b
kostolaza <105263190+kostolaza@users.noreply.github.com>
2022-08-09 21:29:53 -0400

    added search engine

--- src/app.js
+++ src/app.js
@@ -54,9 +54,19 @@ function displayTemperature(response) {
       getForecast(response.data.coord);
 }
 
+function search(city) {
 let apiKey = "8eaa10e0ab5463f9893094121b6b5dc5";
-let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
 
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+  }
+  
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", handleSubmit);
+  
+  search("New York");
